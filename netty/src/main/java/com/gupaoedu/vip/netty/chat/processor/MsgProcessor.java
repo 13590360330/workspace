4c339dc5b8bf89c3c49c1ef8e2b338bbb4d1b2ab
732188486@qq.com <732188486@qq.com>
2021-05-31 11:05:51 +0800

    version 20210531 11:05

--- netty/src/main/java/com/gupaoedu/vip/netty/chat/processor/MsgProcessor.java
+++ netty/src/main/java/com/gupaoedu/vip/netty/chat/processor/MsgProcessor.java
@@ -1,15 +1,23 @@
 package com.gupaoedu.vip.netty.chat.processor;
 
+import com.alibaba.fastjson.JSONObject;
 import com.gupaoedu.vip.netty.chat.protocol.IMDecoder;
 import com.gupaoedu.vip.netty.chat.protocol.IMEncoder;
 import com.gupaoedu.vip.netty.chat.protocol.IMMessage;
+import com.gupaoedu.vip.netty.chat.protocol.IMP;
 import io.netty.channel.Channel;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.AttributeKey;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import org.json.JSONObject;
+import org.json.JSONException;
 
+
+/**
+ * 服务端用户中心
+ * 主要用于自定义协议内容的逻辑处理
+ */
 public class MsgProcessor {
 
     //记录在线用户
@@ -46,13 +54,164 @@ public class MsgProcessor {
         return client.remoteAddress().toString().replaceFirst( "/", "" );
     }
 
-
-    //TODO 改造两个sendMsg方法
-    public void sendMsg(Channel channel, IMMessage msg) {
+    /**
+     * 获取扩展属性
+     *
+     * @param client
+     * @return
+     */
+    public JSONObject getAttrs(Channel client) {
+        try {
+            return client.attr( ATTRS ).get();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public void sendMsg(Channel channel, String msg) {
+    /**
+     * 获取扩展属性
+     *
+     * @param client
+     * @param key
+     * @param value
+     */
+    private void setAttrs(Channel client, String key, Object value) {
+        try {
+            JSONObject json = client.attr( ATTRS ).get();
+            json.put( key, value );
+            client.attr( ATTRS ).set( json );
+        } catch (Exception e) {
+            JSONObject json = new JSONObject();
+            json.put( key, value );
+            client.attr( ATTRS ).set( json );
+        }
     }
 
+    /**
+     * 推出通知
+     *
+     * @param client
+     */
+    public void logout(Channel client) {
 
+        if (getNickName( client ) == null) {
+            return;
+        }
+        for (Channel channel : onlineUsers) {
+            IMMessage request = new IMMessage( IMP.SYSTEM.getName(), sysTime(),
+                    onlineUsers.size(), getNickName( client ) + "离开" );
+            String content = encoder.encode( request );
+            channel.writeAndFlush( new TextWebSocketFrame( content ) );
+        }
+        onlineUsers.remove( client );
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param client
+     * @param msg
+     */
+    public void sendMsg(Channel client, IMMessage msg) {
+        sendMsg( client, encoder.encode( msg ) );
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param client
+     * @param msg
+     */
+    public void sendMsg(Channel client, String msg) {
+        IMMessage request = decoder.decode( msg );
+        if (null == request) {
+            return;
+        }
+
+        String addr = getAddress( client );
+
+        if (request.getCmd().equals( IMP.LOGIN.getName() )) {
+            client.attr( NICK_NAME ).getAndSet( request.getSender() );
+            client.attr( IP_ADDR ).getAndSet( addr );
+            client.attr( FROM ).getAndSet( request.getTerminal() );
+            System.out.println( client.attr( FROM ).get() );
+            onlineUsers.add( client );
+
+            for (Channel channel : onlineUsers) {
+                boolean isself = (channel == client);
+                if (!isself) {
+                    request = new IMMessage( IMP.SYSTEM.getName(), sysTime(),
+                            onlineUsers.size(), getNickName( client ) + "加入" );
+                } else {
+                    request = new IMMessage( IMP.SYSTEM.getName(), sysTime(),
+                            onlineUsers.size(), "已与服务器建立连接" );
+                }
+
+                if ("Console".equals( channel.attr( FROM ).get() )) {
+                    channel.writeAndFlush( request );
+                    continue;
+                }
+                String content = encoder.encode( request );
+                channel.writeAndFlush( new TextWebSocketFrame( content ) );
+            }
+        } else if (request.getCmd().equals( IMP.CHAT.getName() )) {
+            for (Channel channel : onlineUsers) {
+                boolean isself = (channel == client);
+                if (!isself) {
+                    request.setSender( "you" );
+                } else {
+                    request.setSender( getNickName( client ) );
+                }
+                request.setTime( sysTime() );
+
+                if ("Console".equals( channel.attr( FROM ).get() ) & !isself) {
+                    channel.writeAndFlush( request );
+                    continue;
+                }
+                String content = encoder.encode( request );
+                channel.writeAndFlush( new TextWebSocketFrame( content ) );
+            }
+        } else if (request.getCmd().equals( IMP.FLOWER.getName() )) {
+            JSONObject attrs = getAttrs( client );
+            long currTime = sysTime();
+            if (null != attrs) {
+                long lastTime = attrs.getLongValue( "lastFlowerTime" );
+                //60秒内不允许重复送鲜花
+                int secends = 10;
+                long sub = currTime - lastTime;
+                if (sub < 1000 * secends) {
+                    request.setSender( "you" );
+                    request.setCmd( IMP.SYSTEM.getName() );
+                    request.setContent( "您送鲜花太频繁," + (secends - Math.round( sub / 1000 )) + "秒后再试" );
+                    String content = encoder.encode( request );
+                    client.writeAndFlush( new TextWebSocketFrame( content ) );
+                    return;
+                }
+            }
+
+            //正常送花
+            for (Channel channel : onlineUsers) {
+                if (channel == client) {
+                    request.setSender( "you" );
+                    request.setContent( "你给大家送了一波鲜花雨" );
+                    setAttrs( client, "lastFlowerTime", currTime );
+                } else {
+                    request.setSender( getNickName( client ) );
+                    request.setContent( getNickName( client ) + "送来一波鲜花雨" );
+                }
+                request.setTime( sysTime() );
+                String content = encoder.encode( request );
+                channel.writeAndFlush( new TextWebSocketFrame( content ) );
+            }
+        }
+    }
+
+    /**
+     * 获取系统时间
+     *
+     * @return
+     */
+    private long sysTime() {
+        return System.currentTimeMillis();
+    }
 }
