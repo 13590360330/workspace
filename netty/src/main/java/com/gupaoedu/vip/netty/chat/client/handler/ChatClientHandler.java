a5aec8258eb698745f6b449a2222121488908f63
732188486@qq.com <732188486@qq.com>
2021-05-31 15:22:18 +0800

    version 20210531 15:22

--- netty/src/main/java/com/gupaoedu/vip/netty/chat/client/handler/ChatClientHandler.java
+++ netty/src/main/java/com/gupaoedu/vip/netty/chat/client/handler/ChatClientHandler.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 控制台消息处理
@@ -58,19 +60,70 @@ public class ChatClientHandler extends SimpleChannelInboundHandler<IMMessage> {
      * @param ctx
      */
     @Override
-    public void channelActive(ChannelHandlerContext ctx) {
+    public void channelActive(ChannelHandlerContext ctx) throws IOException {
         this.ctx = ctx;
         IMMessage message = new
                 IMMessage( IMP.LOGIN.getName(), "Console", System.currentTimeMillis(), this.nickName );
+        sendMsg( message );
+        log.info( "成功连接服务器,已执行登录动作" );
+        session();
     }
 
-    private boolean sendMsg(IMMessage message) {
-        return false;
+    /**
+     * 发送消息
+     *
+     * @param msg
+     * @return
+     */
+    private boolean sendMsg(IMMessage msg) {
+        ctx.channel().writeAndFlush( msg );
+        System.out.println( "继续输入开始对话..." );
+        return msg.getCmd().equals( IMP.LOGOUT ) ? false : true;
     }
 
-
+    /**
+     * 收到消息后调用
+     *
+     * @param ctx
+     * @param msg
+     * @throws IOException
+     */
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, IMMessage msg) throws Exception {
+    public void channelRead0(ChannelHandlerContext ctx, IMMessage msg) throws IOException {
+        IMMessage m = (IMMessage) msg;
+        System.out.println( (null == m.getSender() ? "" : (m.getSender() + ":")) +
+                removeHtmlTag( m.getContent() ) );
+    }
 
+    public static String removeHtmlTag(String htmlStr) {
+        String regEx_script = "<script[^>]*?>[\\s\\S]*?<\\/script>"; //定义Script的正则表达式
+        String regEx_style = "<style[^>]*?>[\\s\\S]*?<\\/style>"; //定义style的正则表达式
+        String regEx_html = "<[^>]+>"; //定义HTML标签的正则表达式
+
+        Pattern p_script = Pattern.compile( regEx_script, Pattern.CASE_INSENSITIVE );
+        Matcher m_script = p_script.matcher( htmlStr );
+        htmlStr = m_script.replaceAll( "" ); //过滤Script标签
+
+        Pattern p_style = Pattern.compile( regEx_style, Pattern.CASE_INSENSITIVE );
+        Matcher m_style = p_style.matcher( htmlStr );
+        htmlStr = m_style.replaceAll( "" ); //过滤Style标签
+
+        Pattern p_html = Pattern.compile( regEx_html, Pattern.CASE_INSENSITIVE );
+        Matcher m_html = p_html.matcher( htmlStr );
+        htmlStr = m_html.replaceAll( "" ); //过滤HTML标签
+
+        return htmlStr.trim();
+    }
+
+    /**
+     * 发生异常时调用
+     *
+     * @param ctx
+     * @param cause
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.info( "与服务器断开连接:" + cause.getMessage() );
+        ctx.close();
     }
 }
