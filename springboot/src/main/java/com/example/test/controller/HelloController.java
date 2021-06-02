c510cdda03ef2d6fc3132d9a7e97a43f0eb1b6a2
732188486@qq.com <732188486@qq.com>
2021-06-02 09:03:19 +0800

    version 20210601 20:49

---
+++ springboot/src/main/java/com/example/test/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.test.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String sayHello() {
+        return "index";
+    }
+
+    @RequestMapping("/chat")
+    public String chat(){
+        return "chat";
+    }
+}
